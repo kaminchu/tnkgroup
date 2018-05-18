9d23a75ed0054eba983e8d347498674f3a68ba39
kaminchu <kmchu.mail@gmail.com>
2018-05-18 23:57:13 +0900

    initial

--- src/app.js
+++
@@ -1,3 +0,0 @@
-import "./css/style.css";
-import "./js/particle_setting";
-import "./js/render_tnkgroups";
---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom";
+const Application = () => {
+  return <h1>Hello world!</h1>;
+};
+ReactDOM.render(<Application />, document.getElementById("app"));
