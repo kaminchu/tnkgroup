d24e030111a1b4ad6e8e90dbd19bcbf4f2b30f89
kaminchu <kmchu.mail@gmail.com>
2018-05-19 00:07:07 +0900

    add flow

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,8 @@
+// @flow
 import React from "react";
 import ReactDOM from "react-dom";
 const Application = () => {
   return <h1>Hello world!</h1>;
 };
-ReactDOM.render(<Application />, document.getElementById("app"));
+const element = document.getElementById("app");
+element && ReactDOM.render(<Application />, element);
